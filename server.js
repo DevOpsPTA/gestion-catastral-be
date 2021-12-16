0e4fe5fc93cbc29d21518646b05cefa9e7eb2519
Luisa Fernanda <luisafdad@gmail.com>
2021-12-15 23:50:02 -0500

    se adelanta login

--- server.js
+++ server.js
@@ -1,11 +1,28 @@
 const express = require('express');
+const cors = require('cors');
 const app = express();
+
+app.use(cors());
+
+app.use('/login', (req, res) => {
+  res.send({
+    token: 'test123'
+  });
+});
+
 
 // respond with "hello world" when a GET request is made to the homepage
 app.get('/', function(req, res) {
-  res.send('hello world');
-});  
+  res.json('hello world');
+});
+app.get('/predial', function(req, res) {
+    res.json('pago predial')
+  });  
 
-app.listen(3000, () => {
-    console.log("escuchando puert:..", 3000);
+app.post('/predial', function(req, res) {
+    res.json('pago predial')
+  });  
+
+app.listen(3001, () => {
+    console.log("escuchando puert:..", 3001);
 });
